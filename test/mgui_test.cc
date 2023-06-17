f5f9f06be05aea6af85599015db37682b80aa23c
karakirimu <corept634-cmlog@yahoo.co.jp>
2023-06-17 22:30:01 +0900

    [Update] Rename tuple name

--- test/mgui_test.cc
+++ test/mgui_test.cc
@@ -10,10 +10,12 @@ int _byte_index(int x, int y) {
 	return (y >> 3) * WIDTH + x;
 }
 
-typedef std::tuple<int, int, int> P_POS;
+typedef std::tuple<int, int, int> P_A3;
+typedef std::tuple<int, int, int, int> P_A4;
+typedef std::tuple<int, int, int, int, int> P_A5;
 
 class DrawPixelTest :
-    public testing::TestWithParam<P_POS> {};
+    public testing::TestWithParam<P_A3> {};
 
 TEST_P(DrawPixelTest, On) {
   mgui g(WIDTH, HEIGHT);
@@ -30,22 +32,38 @@ INSTANTIATE_TEST_SUITE_P(
   On,
   DrawPixelTest,
   testing::Values(
-    P_POS{0, 0, 1},
-    P_POS{0, 1, 2},
-    P_POS{0, 2, 4},
-    P_POS{0, 3, 8},
-    P_POS{0, 4, 16},
-    P_POS{0, 5, 32},
-    P_POS{0, 6, 64},
-    P_POS{0, 7, 128},
-    P_POS{0, 8, 1},
-    P_POS{1, 0, 1},
-    P_POS{2, 0, 1},
-    P_POS{3, 0, 1},
-    P_POS{4, 0, 1},
-    P_POS{5, 0, 1},
-    P_POS{6, 0, 1},
-    P_POS{7, 0, 1},
-    P_POS{8, 0, 1}
+    P_A3{0, 0, 1},
+    P_A3{0, 1, 2},
+    P_A3{0, 2, 4},
+    P_A3{0, 3, 8},
+    P_A3{0, 4, 16},
+    P_A3{0, 5, 32},
+    P_A3{0, 6, 64},
+    P_A3{0, 7, 128},
+    P_A3{0, 8, 1},
+    P_A3{1, 0, 1},
+    P_A3{2, 0, 1},
+    P_A3{3, 0, 1},
+    P_A3{4, 0, 1},
+    P_A3{5, 0, 1},
+    P_A3{6, 0, 1},
+    P_A3{7, 0, 1},
+    P_A3{8, 0, 1}
   )
 );
+
+class DrawRectangleRoundedTest :
+    public testing::TestWithParam<P_A5> {};
+
+TEST_P(DrawRectangleRoundedTest, On) {
+  mgui g(WIDTH, HEIGHT);
+  int x0 = std::get<0>(GetParam());
+  int y0 = std::get<1>(GetParam());
+  int x1 = std::get<2>(GetParam());
+  int y1 = std::get<3>(GetParam());
+  int r = std::get<4>(GetParam());
+
+  g.draw_rectangle_rounded(x0, y0, x1, y1, r);
+
+  EXPECT_EQ(g.lcd()[_byte_index(x, y)], 0);
+};
