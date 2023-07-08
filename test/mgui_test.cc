a4d483e1809aa83d97bbdc8c9fc02acac02dfb32
karakirimu <37269014+karakirimu@users.noreply.github.com>
2023-07-08 20:36:16 +0900

    [Update] Unit testing

--- test/mgui_test.cc
+++ test/mgui_test.cc
@@ -2,12 +2,22 @@
 #include <gmock/gmock.h>
 
 #include "../mGUI/mgui.h"
+#include <iostream>
 
 constexpr int WIDTH = 128;
 constexpr int HEIGHT = 64;
 
+constexpr int BUFFER_SIZE = (HEIGHT >> 3) * WIDTH;
+
 int _byte_index(int x, int y) {
 	return (y >> 3) * WIDTH + x;
+}
+
+void debug_print(uint8_t *data) {
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+        std::cout << "0x" << std::hex << (unsigned int)data[i] << ",";
+    }
+    std::cout << std::endl;
 }
 
 typedef std::tuple<int, int, int> P_A3;
@@ -26,29 +36,48 @@ TEST_P(DrawPixelTest, On) {
   g.draw_pixel(x, y, true);
 
   EXPECT_EQ(g.lcd()[_byte_index(x, y)], ex);
+
+  // debug_print(g.lcd());
 };
 
 INSTANTIATE_TEST_SUITE_P(
   On,
   DrawPixelTest,
   testing::Values(
-    P_A3{0, 0, 1},
-    P_A3{0, 1, 2},
-    P_A3{0, 2, 4},
-    P_A3{0, 3, 8},
-    P_A3{0, 4, 16},
-    P_A3{0, 5, 32},
-    P_A3{0, 6, 64},
-    P_A3{0, 7, 128},
-    P_A3{0, 8, 1},
-    P_A3{1, 0, 1},
-    P_A3{2, 0, 1},
-    P_A3{3, 0, 1},
-    P_A3{4, 0, 1},
-    P_A3{5, 0, 1},
-    P_A3{6, 0, 1},
-    P_A3{7, 0, 1},
-    P_A3{8, 0, 1}
+    P_A3{0, 0, 128},
+    P_A3{0, 1, 64},
+    P_A3{0, 2, 32},
+    P_A3{0, 3, 16},
+    P_A3{0, 4, 8},
+    P_A3{0, 5, 4},
+    P_A3{0, 6, 2},
+    P_A3{0, 7, 1},
+    P_A3{0, 8, 128},
+    P_A3{1, 0, 128},
+    P_A3{2, 0, 128},
+    P_A3{3, 0, 128},
+    P_A3{4, 0, 128},
+    P_A3{5, 0, 128},
+    P_A3{6, 0, 128},
+    P_A3{7, 0, 128},
+    P_A3{8, 0, 128},
+    P_A3{1, 1, 64},
+    P_A3{2, 2, 32},
+    P_A3{3, 3, 16},
+    P_A3{4, 4, 8},
+    P_A3{5, 5, 4},
+    P_A3{6, 6, 2},
+    P_A3{7, 7, 1},
+    P_A3{8, 8, 128},
+    P_A3{56, 56, 128},
+    P_A3{57, 57, 64},
+    P_A3{58, 58, 32},
+    P_A3{59, 59, 16},
+    P_A3{60, 60, 8},
+    P_A3{61, 61, 4},
+    P_A3{62, 62, 2},
+    P_A3{63, 63, 1},
+    P_A3{64, 0, 128}
   )
 );
 
@@ -64,6 +93,60 @@ TEST_P(DrawRectangleRoundedTest, On) {
   int r = std::get<4>(GetParam());
 
   g.draw_rectangle_rounded(x0, y0, x1, y1, r);
-
-  EXPECT_EQ(g.lcd()[_byte_index(x, y)], 0);
+  // debug_print(g.lcd()); // OK
 };
+
+INSTANTIATE_TEST_SUITE_P(
+  On,
+  DrawRectangleRoundedTest,
+  testing::Values(
+    P_A5{2, 2, 12, 12, 2},
+    P_A5{0, 0, 31, 31, 8}
+  )
+);
+
+class DrawCircleTest :
+    public testing::TestWithParam<P_A3> {};
+
+TEST_P(DrawCircleTest, On) {
+    mgui g(WIDTH, HEIGHT);
+    int x0 = std::get<0>(GetParam());
+    int y0 = std::get<1>(GetParam());
+    int r = std::get<2>(GetParam());
+
+    g.draw_circle(x0, y0, r);
+    // debug_print(g.lcd()); // OK
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    On,
+    DrawCircleTest,
+    testing::Values(
+        P_A3{ 8, 8, 6 },
+        P_A3{ 32, 32, 20 }
+    )
+);
+
+class DrawRectangleTest :
+    public testing::TestWithParam<P_A5> {};
+
+TEST_P(DrawRectangleTest, On) {
+    mgui g(WIDTH, HEIGHT);
+    int x0 = std::get<0>(GetParam());
+    int y0 = std::get<1>(GetParam());
+    int x1 = std::get<2>(GetParam());
+    int y1 = std::get<3>(GetParam());
+    bool fill = std::get<4>(GetParam());
+
+    g.draw_rectangle(x0, y0, x1, y1, fill);
+    // debug_print(g.lcd()); // OK
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    On,
+    DrawRectangleTest,
+    testing::Values(
+        P_A5{ 0, 0, 15, 15, true },
+        P_A5{ 4, 4, 24, 24, false }
+    )
+);
